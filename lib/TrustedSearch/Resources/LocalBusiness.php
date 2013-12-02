185c9228d2c08d3c9da271d4c8382c9f8b187290
Mark Evans <mrkevans@gmail.com>
2013-12-02 13:58:46 -0800

    update method docs

--- lib/TrustedSearch/Resources/LocalBusiness.php
+++ lib/TrustedSearch/Resources/LocalBusiness.php
@@ -2,6 +2,13 @@
 
 class TrustedSearch_LocalBusiness extends TrustedSearch_ApiResource{
 
+	/**
+	 * Create a new locations for fulfillment.
+	 * @param  array $data          an array of location orders. 
+	 * @param  string $apiPublicKey  [optional public key]
+	 * @param  string $apiPrivateKey [optional private key]
+	 * @return TrustedSearch LocalBusiness Object
+	 */
 	public static function create($data=null, $apiPublicKey=null, $apiPrivateKey=null)
 	{
 	  $class = get_class();
