6c5c0fd537e58edc324cdf76ca2f5d789428f80d
Mark Evans <mrkevans@gmail.com>
2013-12-03 00:00:35 -0800

    UPdate Token to use new ->getData method

--- lib/TrustedSearch/Resources/Token.php
+++ lib/TrustedSearch/Resources/Token.php
@@ -13,9 +13,9 @@ class TrustedSearch_Token extends TrustedSearch_ApiResource
    */
   public static function get($username, $password, $apiPublicKey=null, $apiPrivateKey=null){
     $class = get_class();
-    $path = array('tokens', $username);
+    $path = array('token', $username);
     $params = array('pwd'=>$password);
-    return self::_customRetrieve($class, $path, $params, $apiPublicKey, $apiPrivateKey);
+    return self::_get($class, $path, $params, $apiPublicKey, $apiPrivateKey);
   }
 
   
